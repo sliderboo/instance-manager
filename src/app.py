80fba256789a6e4e2d606154096f3cc7fe70cd3b
anhhung04 <dev.hah4@gmail.com>
2024-07-06 23:26:44 +0700

    faet: auth

--- src/app.py
+++ src/app.py
@@ -2,25 +2,39 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from config import config
+from repository.schema import Base
+from repository.schema.user import User  # noqa
+from sqlalchemy import create_engine
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(docs_url='/api/docs' if not config['PROD'] else None,
-              redoc_url='/api/redoc' if not config['PROD'] else None,
-              openapi_url='/api/openapi.json' if not config['PROD'] else None)
+app = FastAPI(
+    docs_url="/api/docs" if not config["PROD"] else None,
+    redoc_url="/api/redoc" if not config["PROD"] else None,
+    openapi_url="/api/openapi.json" if not config["PROD"] else None,
+)
 
 for file in os.listdir("routes"):
-    if not file.startswith('_'):
+    if not file.startswith("_"):
         module_name, _ = os.path.splitext(file)
         module = __import__(f"routes.{module_name}", fromlist=[module_name])
         app.include_router(module.router, prefix=f"/api/{module_name}")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=config["ALLOWED_ORIGINS"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     configs = {
-        'host': config['HOST'],
-        'port': config['PORT'],
-        'reload': not config['PROD'],
-        'workers': config['workers'] if config['PROD'] else 1
+        "host": config["HOST"],
+        "port": config["PORT"],
+        "reload": not config["PROD"],
+        "workers": config["workers"] if config["PROD"] else 1,
     }
+    Base.metadata.create_all(bind=create_engine(config["DATABASE_URL"]))
     uvicorn.run("app:app", **configs)
 
 __all__ = ["app"]
---
+++ src/utils/crypto.py
@@ -0,0 +1,104 @@
+from repository import RedisStorage
+from fastapi import Depends, HTTPException, Cookie
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from typing import Optional, Tuple, Annotated, Union
+from hashlib import sha256, pbkdf2_hmac
+from hmac import new
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+from json import dumps, loads
+from string import ascii_letters, digits
+from random import choices
+from redis import Redis
+from config import config
+
+
+class PasswordHandler:
+    @staticmethod
+    def hash(password: str) -> str:
+        return urlsafe_b64encode(pbkdf2_hmac("sha256", password.encode(), config["PASSWORD_SALT"].encode(), 100000)).decode()
+
+    @staticmethod
+    def verify(password: str, hashed: str) -> bool:
+        return hashed == PasswordHandler.hash(password)
+
+
+class Base64:
+    @staticmethod
+    def encode(data: str) -> str:
+        return urlsafe_b64encode(data.encode()).decode().rstrip('=')
+
+    @staticmethod
+    def decode(data: str) -> str:
+        enc = data + '=' * (-len(data) % 4)
+        return urlsafe_b64decode(enc.encode()).decode()
+
+
+class JWTHandler:
+    def __init__(self, store: Redis = Depends(RedisStorage.get)):
+        self._store = store
+
+    def verify(self, token: str) -> Tuple[Optional[dict], Exception]:
+        _, payload, signature = token.split(".")
+        try:
+            payload = loads(Base64.decode(payload))
+            uid = payload.get("uid", None)
+            assert uid is not None
+            secret = self._store.get(uid)
+            if not secret:
+                raise Exception("Invalid or expired token")
+            if isinstance(secret, bytes):
+                secret = secret.decode()
+            assert signature == self.sign(payload, secret), "Invalid signature"
+            return payload, None
+        except Exception as e:
+            return None, Exception(str(e))
+
+    def create(self, payload: dict) -> str:
+        try:
+            secret = "".join(choices(ascii_letters + digits, k=64))
+            uid = payload.get("uid", None)
+            assert uid is not None
+            self._store.set(uid, secret, ex=config["JWT_EXPIRATION"])
+            return ".".join([
+                Base64.encode(dumps({"alg": "HS256", "typ": "JWT"})),
+                Base64.encode(dumps(payload)),
+                self.sign(payload, secret)
+            ])
+        except Exception as e:
+            return str(e)
+
+    def revoke(self, uid: str) -> None:
+        self._store.delete(uid)
+
+    @staticmethod
+    def sign(payload: dict, secret: str) -> str:
+        payload = dumps({"alg": "HS256", "typ": "JWT"}) + "." + dumps(payload)
+        return Base64.encode(new(secret.encode(), payload.encode(), sha256).hexdigest())
+
+
+def get_cookie_token(token: Annotated[Union[None, str], Cookie(alias="auth")] = None) -> Optional[str]:
+    return token
+
+
+def auth(
+    token: HTTPAuthorizationCredentials = Depends(
+        HTTPBearer(auto_error=False)
+    ),
+    jwt_handler: JWTHandler = Depends(JWTHandler),
+    cookie_token: str = Depends(get_cookie_token),
+):
+    if not token:
+        cred = cookie_token
+    else:
+        cred = token.credentials
+    if not cred:
+        raise Exception("No token provided")
+    try:
+        payload, error = jwt_handler.verify(cred)
+        assert error is None, str(error)
+        return payload
+    except Exception as e:
+        raise HTTPException(
+            status_code=403,
+            detail=str(e)
+        )
